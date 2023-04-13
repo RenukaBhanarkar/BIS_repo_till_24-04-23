684b1831c6c8427221cfc417963bdade98bf2cd0
Akshay <sakhare.akshay51@gmail.com>
2023-04-13 18:53:10 +0530

    by the mentor changes

--- application/views/users/by_the_montor_list.php
+++ application/views/users/by_the_montor_list.php
@@ -27,18 +27,20 @@
                     </div>
                 </div>
             </div>
-        <?php } ?>
-        <div class="row">
-    <!-- <div class="col-12"> -->
-
-        <?php
-        if ($this->session->flashdata('MSG')) {
-            echo $this->session->flashdata('MSG');
-        }
-        ?>
-        
-            <div class="col-12 mt-3">
-                <div class="card border-top card-body">
+        <?php }  ?>
+        <div class="row" style="padding:10px;">
+        <div class="card p-3 shadow">
+                <nav>
+                    <div class="nav nav-tabs mb-3" id="nav-tab" role="tablist">
+                        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">New Requests</button>
+                        <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Approve Requests</button>
+                        <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Rejected Requests</button>
+                    </div>
+                </nav>
+            <div class="tab-content p-3 border bg-light" id="nav-tabContent">
+              <div class="tab-pane fade active show" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                <div class="col-12 mt-3">
+                   <div class="card border-top card-body">
                     <table id="example" class="table table-bordered">
                         <thead>
                             <tr>
@@ -52,9 +54,71 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <?php if (!empty($bythementors)) {
+                            <?php if (!empty($created)) {
                                 $i = 1;
-                                foreach ($bythementors as $list_btm) { ?>
+                                foreach ($created as $list_btm1) { ?>
+
+                                    <tr>
+                                        <td><?php echo $i++ ?></td>
+                                        <td><?php echo $list_btm1['title']; ?></td>                                        
+                                        <td><?php echo $list_btm1['created_on']; ?></td>                                        
+                                        <td><?php if ($list_btm1['image']) { ?>
+                                                <img src="<?php echo base_url(); ?>uploads/by_the_mentors/img/<?php echo $list_btm1['image'] ?>" data-toggle="modal" data-target="#viewImage" width="40px">
+                                            <?php } else {
+                                                echo "No Uploaded";
+                                            } ?>
+                                        </td>
+                                        <td style="text-align: center;"><?php if(empty($list_btm1['document'])){ echo "-"; }else{?>
+                                            <a href="<?php echo base_url().'uploads/by_the_mentors/doc/'.$list_btm1['document']; ?>" target="_blank"><img  src="<?php echo base_url().'uploads/pdf.png'; ?>" width="25px"/></a>
+                                            <?php } ?>
+                                        </td>
+                                        <td><?php echo $list_btm1['status_name']; ?> </td>
+
+                                        <td class="d-flex border-bottom-0">
+                                            <!-- <button onClick="" class="btn btn-info btn-sm mr-2 text-white" data-toggle="modal"
+                                        data-target="#editform"><i class="fa fa-edit" aria-hidden="true"></i></button> -->
+                                        <a href="<?php echo base_url(); ?>admin/view_btm/<?php echo $list_btm1['id'] ?>"><button class="btn btn-info btn-sm mr-2">view</button></a>
+                                            <?php if($list_btm1['status'] == '1'){ ?>
+                                                <button onclick="deleteByTheMentor(' <?php echo $list_btm1['id']; ?> ');" data-id='<?php echo $list_btm1['id']; ?>' class="btn btn-danger btn-sm mr-2 delete_img">Delete</button>
+                                                <!-- <button class="btn btn-primary btn-sm mr-2" onclick="sendPublish('<?php echo $list_btm1['id']; ?>')" data-id ='<?php echo $list_btm1['id']; ?>'>Publish</button> -->
+                                                <button onclick="approve('<?php echo $list_btm1['id']; ?>')" class="btn btn-success btn-sm mr-2 text-white">Approve</button>
+                                                <!-- <button class="btn btn-primary btn-sm" onclick="sendReject('<?php echo $list_btm1['id']; ?>')" data-id ='<?php echo $list_btm1['id']; ?>'>Reject</button> -->
+                                                <button onclick="reject('<?php echo $list_btm1['id']; ?>')" class="btn btn-danger btn-sm mr-2 text-white">Reject</button>
+                                                <!-- <button class="btn btn-primary btn-sm ml-2" onclick="sendArchive('<?php echo $list_btm1['id']; ?>')" data-id ='<?php echo $list_btm['id']; ?>'>Archive</button> -->
+                                          
+                                                <!-- <button class="btn btn-primary btn-sm" onclick="sendUnPublish('<?php echo $list_btm1['id']; ?>')" data-id ='<?php echo $list_btm1['id']; ?>'>UnPublish</button> -->
+                                         <?php } ?>
+                                        
+                                            <!-- Modal -->
+                                            
+                                    </tr>
+                            <?php }
+                            } ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            </div>
+            <div class="tab-pane fade active show" id="nav-profile" role="tabpanel" aria-labelledby="nav-home-tab">
+                <div class="col-12 mt-3">
+                   <div class="card border-top card-body">
+                    <table id="example1" class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Sr. No.</th>
+                                <th>Title</th>
+                                <th>Created On</th>
+                                <th>Image</th>
+                                <th>Document</th>                                
+                                <th>Status</th>
+                                <th>2</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if (!empty($approved)) {
+                                $i = 1;
+                                foreach ($approved as $list_btm) { ?>
 
                                     <tr>
                                         <td><?php echo $i++ ?></td>
@@ -74,8 +138,8 @@
                                         data-target="#editform"><i class="fa fa-edit" aria-hidden="true"></i></button> -->
                                         <a href="<?php echo base_url(); ?>admin/view_btm/<?php echo $list_btm['id'] ?>"><button class="btn btn-info btn-sm mr-2">view</button></a>
                                             <?php if(!($list_btm['status_name'] == 'Published')){ ?>
-                                                <button onclick="deleteByTheMentor(' <?php echo $list_btm['id']; ?> ');" data-id='<?php echo $list_btm['id']; ?>' class="btn btn-danger btn-sm mr-2 delete_img">Delete</button>
-                                                <button class="btn btn-primary btn-sm" onclick="sendPublish('<?php echo $list_btm['id']; ?>')" data-id ='<?php echo $list_btm['id']; ?>'>Publish</button>
+                                                <!-- <button onclick="deleteByTheMentor(' <?php echo $list_btm['id']; ?> ');" data-id='<?php echo $list_btm['id']; ?>' class="btn btn-danger btn-sm mr-2 delete_img">Delete</button> -->
+                                                <button class="btn btn-success btn-sm" onclick="sendPublish('<?php echo $list_btm['id']; ?>')" data-id ='<?php echo $list_btm['id']; ?>'>Publish</button>
                                                 <button class="btn btn-primary btn-sm ml-2" onclick="sendArchive('<?php echo $list_btm['id']; ?>')" data-id ='<?php echo $list_btm['id']; ?>'>Archive</button>
                                             <?php }else if($list_btm['status_name'] == 'Published'){ ?>
                                                 <button class="btn btn-primary btn-sm" onclick="sendUnPublish('<?php echo $list_btm['id']; ?>')" data-id ='<?php echo $list_btm['id']; ?>'>UnPublish</button>
@@ -91,6 +155,67 @@
                     </table>
                 </div>
             </div>
+            </div>
+            <div class="tab-pane fade active show" id="nav-contact" role="tabpanel" aria-labelledby="nav-home-tab">
+                <div class="col-12 mt-3">
+                   <div class="card border-top card-body">
+                    <table id="example2" class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Sr. No.</th>
+                                <th>Title</th>
+                                <th>Created On</th>
+                                <th>Image</th>
+                                <th>Document</th>                                
+                                <th>Status</th>
+                                <th>Reject Reason</th>
+                                <th>3</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if (!empty($rejected)) {
+                                $i = 1;
+                                foreach ($rejected as $list_btm) { ?>
+
+                                    <tr>
+                                        <td><?php echo $i++ ?></td>
+                                        <td><?php echo $list_btm['title']; ?></td>                                        
+                                        <td><?php echo $list_btm['created_on']; ?></td>                                        
+                                        <td><?php if ($list_btm['image']) { ?>
+                                                <img src="<?php echo base_url(); ?>uploads/by_the_mentors/img/<?php echo $list_btm['image'] ?>" data-toggle="modal" data-target="#viewImage" width="40px">
+                                            <?php } else {
+                                                echo "No Uploaded";
+                                            } ?>
+                                        </td>
+                                        <td style="text-align: center;"><a href="<?php echo base_url().'uploads/by_the_mentors/doc/'.$list_btm['document']; ?>" target="_blank"><img  src="<?php echo base_url().'uploads/pdf.png'; ?>" width="25px"/></a></td>
+                                        <td><?php echo $list_btm['status_name']; ?> </td>
+                                        <td><?php echo $list_btm['reject_reason']; ?></td>
+
+                                        <td class="d-flex border-bottom-0">
+                                            <!-- <button onClick="" class="btn btn-info btn-sm mr-2 text-white" data-toggle="modal"
+                                        data-target="#editform"><i class="fa fa-edit" aria-hidden="true"></i></button> -->
+                                        <a href="<?php echo base_url(); ?>admin/view_btm/<?php echo $list_btm['id'] ?>"><button class="btn btn-info btn-sm mr-2">view</button></a>
+                                           
+                                                <button onclick="deleteByTheMentor(' <?php echo $list_btm['id']; ?> ');" data-id='<?php echo $list_btm['id']; ?>' class="btn btn-danger btn-sm mr-2 delete_img">Delete</button>
+                                                <!-- <button class="btn btn-primary btn-sm" onclick="sendPublish('<?php echo $list_btm['id']; ?>')" data-id ='<?php echo $list_btm['id']; ?>'>Publish</button> -->
+                                                <button class="btn btn-primary btn-sm ml-2" onclick="sendArchive('<?php echo $list_btm['id']; ?>')" data-id ='<?php echo $list_btm['id']; ?>'>Archive</button>
+                                          
+                                                <!-- <button class="btn btn-primary btn-sm" onclick="sendUnPublish('<?php echo $list_btm['id']; ?>')" data-id ='<?php echo $list_btm['id']; ?>'>UnPublish</button> -->
+                                 
+                                        
+                                            <!-- Modal -->
+                                            
+                                    </tr>
+                            <?php }
+                            } ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            </div>
+          </div>
+        </div>
         </div>
     <!-- </div> -->
     <!-- /.container-fluid -->
@@ -177,13 +302,72 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="approve" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Approve Activity</h5>
+                    <button class="close" type="button" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to Approve activity?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary approve" data-bs-dismiss="modal">Approve</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="rejectModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title fs-5" id="exampleModalLabel">Reject</h5>
+                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                    <button class="close" type="button" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="<?php echo base_url(); ?>admin/rejectbtTheMentor"  method="post" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Reason of Rejection<sup class="text-danger">*</sup></label>
+                            <!-- <input type="text" class="form-control" id="reseon" name="reason" required> -->
+                            <textarea class="form-control" id="reseon" name="reason" required></textarea>
+                            <input type="hidden" id="id2" name="id">
+                            <span id="err_title" class="text-danger"></span>
+                        </div>
+                       
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-danger" >Reject</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 <!-- End of Main Content -->
 <script>
-    // $(document).ready(function(){
-    //     $('.delete_img').on('click',function(){
-    //         $('#delete').modal('show');
-    //     })
-    // })
+    $(document).ready(function(){
+        // $('.delete_img').on('click',function(){
+        //     $('#delete').modal('show');
+        // })
+        
+        $('#example1').DataTable();
+        $('#example2').DataTable();
+    })
+    function reject(que_id){
+            $('#rejectModal1').modal('show');
+            $('#id2').val(que_id);
+        }
     function deleteByTheMentor(que_id) {
         // var c = confirm("Are you sure to delete this survey details? ");
         $('#delete').modal('show');
@@ -204,6 +388,31 @@
             });
         });
     }
+
+    function approve(que_id){
+            // var c = confirm("Are you sure to Approve activity? ");
+            // if (c == true) {
+                $('#approve').modal('show');
+                $('.approve').on('click', function() {
+                // const $loader = $('.igr-ajax-loader');
+                //$loader.show();
+                $.ajax({
+                    type: 'POST',
+                    url: '<?php echo base_url(); ?>admin/approve_btm',
+                    data: {
+                        que_id: que_id,
+                    },
+                    success: function(result) {
+
+                        location.reload();
+                    },
+                    error: function(result) {
+                        alert("Error,Please try again.");
+                    }
+                });
+
+            })
+        }
 
     function sendArchive(que_id) {
             // var c = confirm("Are you sure to Publish By The Mentor details? ");
@@ -232,6 +441,34 @@
 
             });
         }
+        
+        // function sendReject(que_id) {
+        //     // var c = confirm("Are you sure to Publish By The Mentor details? ");
+        //     // if (c == true) {
+        //         // const $loader = $('.igr-ajax-loader');
+        //         //$loader.show();
+        //         // $('#rejectModal1').modal('show');
+        //         $('.archive').on('click', function() {
+        //         $.ajax({
+        //             type: 'POST',
+        //             url: '<?php echo base_url(); ?>admin/sendReject',
+        //             data: {
+        //                 que_id: que_id,
+        //             },
+        //             success: function(result) {
+        //                 // $('#row' + que_id).css({
+        //                 //     'display': 'none'
+        //                 // });
+        //                 // alert('success' 'refresh');
+        //                 location.reload();
+        //             },
+        //             error: function(result) {
+        //                 alert("Error,Please try again.");
+        //             }
+        //         });
+
+        //     });
+        // }
 
     function sendPublish(que_id) {
             // var c = confirm("Are you sure to Publish By The Mentor details? ");
