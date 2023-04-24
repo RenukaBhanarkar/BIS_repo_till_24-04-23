008956d196f46286393bd6ab92cbecd581a68b1e
AnisMulani16 <126258106+AnisMulani16@users.noreply.github.com>
2023-04-24 19:58:50 +0530

    changes

--- application/views/quiz/answer_key_list.php
+++ application/views/quiz/answer_key_list.php
@@ -15,118 +15,114 @@
             </nav>
                         
                     </div>
+                    <div class="row">
+                        <div class="col-md-12" style="text-align: end;">
+                    <a class="btn btn-primary btn-sm mr-2" >Export as PDF</a>
+                    <a class="btn btn-info btn-sm mr-2" >Print</a>
+                    </div>
+                    </div>
 <!-- Content Row -->
 <div class="row">
                         <div class="col-12 mt-3">
                         <div class="card border-top">
                            <div class="card-body"> 
                             <div class="row">
-                               <div class="mb-2 col-md-4">
-                                    <label class="d-block text-font">Name of Quiz</label>
-                                    <div>
-                                        <p>Indpendance Quiz</p>
+                               <div class="mb-2 col-md-12 d-flex">
+                                    <label class="d-block text-font">Question 1 :</label>
+                                    <div class="ml-2">
+                                        <p>What is the color of Lion ?</p>
                                     </div>    
                                 </div>
-                                <div class="mb-2 col-md-4">
-                                    <label class="d-block text-font">Quiz Id</label>
+                                </div>
+                                <div class="row">
+                                <div class="mb-2 col-md-12">
+                                    <label class="d-block text-font">Options :</label>
                                     <div>
-                                        <p>1234</p>
+                                        <ol>
+                                            <li>Orange</li>
+                                            <li>Red</li>
+                                            <li>Yellow</li>
+                                            <li>Blue</li>
+                                            <li>Pink</li>
+                                        </ol>    
+                                        <!-- <p>Orange</p>
+                                        <p>Red</p>
+                                        <p>Yellow</p>
+                                        <p>blue</p>
+                                        <p>Pink</p> -->
                                     </div>    
                                 </div>
-                                <div class="mb-2 col-md-4">
-                                    <label class="d-block text-font">Quiz Date</label>
-                                    <div>
-                                        <p>12/03/2023</p>
+                             </div>
+                            <div class="row">
+                                <div class="mb-2 col-md-8 d-flex">
+                                    <label class="d-block text-font">Correct Option :</label>
+                                    <div class="ml-2">
+                                        <p>5</p>
                                     </div>    
                                 </div>
-                                <div class="mb-2 col-md-4">
-                                    <label class="d-block text-font">Total Marks</label>
-                                    <div>
+                                <div class="mb-2 col-md-4 d-flex">
+                                    <label class="d-block text-font">Option Select :</label>
+                                    <div class="ml-2">
                                         <p>100</p>
-                                    </div>    
-                                </div>
-                                <div class="mb-2 col-md-4">
-                                    <label class="d-block text-font">Total Submission</label>
-                                    <div>
-                                        <p>100</p>
-                                    </div>    
-                                </div>
-                                <div class="mb-2 col-md-4">
-                                    <label class="d-block text-font">Total Winners</label>
-                                    <div>
-                                        <p>50</p>
-                                    </div>    
-                                </div>
-                                <div class="mb-2 col-md-4">
-                                    <label class="d-block text-font">Declared on</label>
-                                    <div>
-                                        <p>IT Services Department</p>
                                     </div>    
                                 </div>
                             </div>
-<div class="row">
-            <div class="col-12 mt-3">
-                <table id="example" class="table-bordered display nowrap" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>Sr. No.</th>
-                            <th>Name</th>
-                            <th>Email ID</th>
-                            <th>Contact No.</th>
-                            <th>Date</th>
-                            <th>Time</th>
-                            <th>Score</th>
-                            <th>Prize</th>
+                            <hr>
+                            <div class="row">
+                                <div class="mb-2 col-md-6">
+                                    <label class="d-block text-font">Question 2:</label>
+                                    <div>
+                                        <p>Which Symbol is this ?</p>
+                                    </div>
+                                    <div>
+                                        <img src="http://localhost/BIS/BIS_repo/assets/admin/img/bis_logo.png" alt="" class="" width="100%" style="height:227px">
+                                    </div>    
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="mb-2 col-md-12">
+                                    <label class="d-block text-font">Option :</label>
+                                    <div class="row">
+                                            <div class="col-md-6">
+                                              <img src="http://localhost/BIS/BIS_repo/assets/admin/img/quiz_img.jpeg" alt="" class="" width="100%" style="height:181px">
+                                            </div>
+                                            <div class="col-md-6">
+                                              <img src="http://localhost/BIS/BIS_repo/assets/admin/img/quiz_img.jpeg" alt="" class="" width="100%" style="height:181px">
+                                            </div>
+                                            </div>
+                                        <div class="row mt-2">    
+                                            <div class="col-md-6">
+                                              <img src="http://localhost/BIS/BIS_repo/assets/admin/img/quiz_img.jpeg" alt="" class="" width="100%" style="height:181px">
+                                            </div> 
+                                            <div class="col-md-6">
+                                              <img src="http://localhost/BIS/BIS_repo/assets/admin/img/quiz_img.jpeg" alt="" class="" width="100%" style="height:181px">
+                                            </div> 
+                                    </div>   
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="mb-2 col-md-8 d-flex">
+                                    <label class="d-block text-font">Correct Option :</label>
+                                    <div class="ml-2">
+                                        <p>5</p>
+                                    </div>    
+                                </div>
+                                <div class="mb-2 col-md-4 d-flex">
+                                    <label class="d-block text-font">Option Select :</label>
+                                    <div class="ml-2">
+                                        <p>100</p>
+                                    </div>    
+                                </div>
+                            </div>
                             
-                        </tr>
-                    </thead>
-                    <tbody>
-                      
 
-                                <tr>
-                                 <td>1</td>
-                                 <td>Name</td>
-                                 <td>abc@gmail.com</td>
-                                 <td>7057085889</td> 
-                                 <td>12/03/2023</td> 
-                                 <td>12:00:00</td> 
-                                 <td>100</td>
-                                 <td>First Prize</td>
-                                  
-                                 </tr>
-
-                           
-                        
-                        
-                           
-                    </tbody>
-                </table>
-            </div>
-        </div>
                           </div>
                           <div class="col-md-12 submit_btn p-3">
-                               <a class="btn btn-primary btn-sm text-white" onclick="location.href='<?php echo base_url();?>admin/admin_creation_list'">Back</a>
+                               <a class="btn btn-primary btn-sm text-white" onclick="location.href='<?php echo base_url();?>Quiz/closed_quiz_submission'">Back</a>
                           </div>  
-                           <!-- Modal -->
-                           <div class="modal fade" id="cancelForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                              <div class="modal-content">
-                                <div class="modal-header">
-                                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>Are you sure you want to cancel?</p>
-                                </div>
-                                <div class="modal-footer">
-                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
-                                </div>
-                              </div>
-                            </div>
-                          </div>       
-                                      <!-- Modal -->
-                        </div> 
+                                                   </div> 
                       </div>
                     </div>
                     </div>
